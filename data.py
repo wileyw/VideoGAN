1fa7be155ad07499854e9b741bf05a6d2899e3b2
jinac <jinacay018@gmail.com>
2018-10-14 00:22:49 -0700

    Adding in-progress data utils

---
+++ data.py
@@ -0,0 +1,89 @@
+import os
+import torch
+import torchvision
+import glob
+import matplotlib.pyplot as plt
+import numpy as np
+import torch.optim as optim
+
+from skimage import io, transform
+from skimage.transform import resize
+
+import d_net
+import g_net
+
+class PacmanDataset(torch.utils.data.Dataset):
+    def __init__(self, videos_dir):
+        self.videos_dir = videos_dir
+        image_dirs = glob.glob(os.path.join(self.videos_dir, '*'))
+
+        self.before_image_paths = []
+        self.after_image_paths = []
+        for image_dir in image_dirs:
+            image_paths = sorted(glob.glob(os.path.join(image_dir, '*.png')))
+
+            for i in range(len(image_paths) - 1):
+                self.before_image_paths.append(image_paths[i])
+                self.after_image_paths.append(image_paths[i + 1])
+
+    def __len__(self):
+        return len(self.before_image_paths)
+
+    def __getitem__(self, idx):
+        image_path0 = self.before_image_paths[idx]
+        image0 = io.imread(image_path0)
+        image0 = resize(image0, (32, 32), anti_aliasing=True)
+        image0 = np.rollaxis(image0, 2, 0) / 255.
+
+        image_path1 = self.after_image_paths[idx + 1]
+        image1 = io.imread(image_path1)
+        image1 = resize(image1, (32, 32), anti_aliasing=True)
+        image1 = np.rollaxis(image1, 2, 0) / 255.
+
+        return {'image0': image0, 'image1': image1}
+
+def main():
+    #dataset = PacmanDataset('Ms_Pacman/Train/')
+    dataset = PacmanDataset('Ms_Pacman/Test/')
+    dataset_loader = torch.utils.data.DataLoader(dataset, batch_size=5, shuffle=False, num_workers=1)
+
+    SCALE_CONV_FSM_D = [[3, 64],
+            [3, 64, 128, 128],
+            [3, 128, 256, 256],
+            [3, 128, 256, 512, 128]]
+    SCALE_KERNEL_SIZES_D = [[3],
+            [3, 3, 3],
+            [5, 5, 5],
+            [7, 7, 5, 5]]
+    SCALE_FC_LAYER_SIZES_D = [[512, 256, 1],
+            [1024, 512, 1],
+            [1024, 512, 1],
+            [1024, 512, 1]]
+
+    D = d_net.DiscriminatorModel(kernel_sizes_list=SCALE_KERNEL_SIZES_D,
+            conv_layer_fms_list=SCALE_CONV_FSM_D,
+            scale_fc_layer_sizes_list=SCALE_FC_LAYER_SIZES_D)
+
+    G = g_net.GeneratorModel()
+
+    optimizer = optim.SGD(D.parameters(), lr=0.001, momentum=0.9)
+
+    for epoch in range(1):
+        for i_batch, sample_batch in enumerate(dataset_loader):
+            before_batch = sample_batch['image0'].float()
+            after_batch = sample_batch['image1'].float()
+
+            generated_image = G(before_batch)
+            print(generated_image)
+            exit()
+
+            result = D(generated_image)
+
+            print(result)
+            print(result.shape)
+
+            print(i_batch, before_batch.shape, after_batch.shape)
+            exit()
+
+if __name__ == '__main__':
+    main()
---
+++ process_data.py
@@ -0,0 +1,89 @@
+"""
+Adapted from https://github.com/dyelax/Adversarial_Video_Generation/blob/master/Code/process_data.py
+"""
+
+import argparse
+import glob
+
+import numpy as np
+
+import data_util
+
+
+def clip_l2_diff(clip, hist_len):
+    """
+    """
+    diff = 0
+    for i in xrange(hist_len):
+        frame = clip[:, :, 3 * i:3 * (i + 1)]
+        next_frame = clip[:, :, 3 * (i + 1):3 * (i + 2)]
+        diff += np.sum(np.square(next_frame - frame))
+
+    return diff
+
+def process_clip(train_dir, frame_h, frame_w,
+				 hist_len, movement_threshold=100):
+    clip = data_util.get_full_clips(train_dir, hist_len, 1)[0]
+
+    # Randomly crop the clip. With 0.05 probability, take the first crop offered, otherwise,
+    # repeat until we have a clip with movement in it.
+    take_first = np.random.choice(2, p=[0.95, 0.05])
+    cropped_clip = np.empty([frame_h, frame_w, 3 * (hist_len + 1)])
+    for i in xrange(100):  # cap at 100 trials in case the clip has no movement anywhere
+        crop_x = np.random.choice(data_util.FULL_WIDTH - frame_w + 1)
+        crop_y = np.random.choice(data_util.FULL_HEIGHT - frame_h + 1)
+        cropped_clip = clip[crop_y:crop_y + frame_h, crop_x:crop_x + frame_w, :]
+
+        if take_first or clip_l2_diff(cropped_clip, hist_len) > movement_threshold:
+            break
+
+    return cropped_clip
+
+def process_training_data(train_dir, num_clips,
+						  frame_h, frame_w, hist_len,
+						  movement_threshold, clip_dir):
+	num_prev_clips = len(glob(clip_dir + '*'))
+
+	for clip_num in range(num_prev_clips, num_clips + num_prev_clips):
+		clip = process_clip(train_dir, frame_h, frame_w,
+							hist_len, movement_threshold)
+		np.savez_compresed(os.path.join(clip_dir, str(clip_num)), clip)
+
+		n = clip_num + 1
+		if (n) % 100 == 0:
+			print('Process {} clips'.format(n))
+
+def main():
+	parser = argparse.ArgumentParser(
+		description='Script to process video directory.')
+	parser.add_argument('-n', '--num_clips',
+						default=5000000,
+						help='Number of clips to output for training.')
+	parser.add_argument('-th', '--height',
+						default=32,
+						help='Frame height of training clips.')
+	parser.add_argument('-tw', '--width',
+						default=32,
+						help='Frame width of training clips.')
+	parser.add_argument('-cl', '--hist_len',
+						default=4,
+						help='Length of training episode clips.')
+	parser.add_argument('-t', '--move_th',
+						default=100,
+						help='Movement threshold for l2 diff in considering training clip.')
+	parser.add_argument('train_dir',
+						help='Directory containing full training frames.')
+	parser.add_argument('clip_dir',
+						help=('Output directory for processed training clips.'
+							  "(Make this hidden "
+							  "so the filesystem doesn't freeze with"
+							  "so many files. DON'T `ls` THIS DIRECTORY!)")
+						)
+	args = parser.parse_args()
+	
+	process_training_data(args.train_dir, args.num_clips,
+						  args.height, args.width, args.hist_len,
+						  args.move_th, args.clip_dir)
+
+if __name__ == '__main__':
+	main()
