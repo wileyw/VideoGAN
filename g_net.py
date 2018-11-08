94d8648b44103c3a7229a4cd8ba678ca4d33f8d4
Michael Wang <mswang12@gmail.com>
2018-11-07 19:55:22 -0800

    Removing stride = 2 from generator

--- g_net.py
+++ g_net.py
@@ -48,39 +48,45 @@ class GeneratorDefinitions(nn.Module):
 
         # Generator Net 1
         self.layers1 = nn.ModuleList()
-        self.layers1.append(nn.Conv2d(3, 128, 3, stride=2, padding=1))
-        self.layers1.append(nn.Conv2d(128, 256, 3, stride=2, padding=1))
-        self.layers1.append(nn.ConvTranspose2d(256, 128, 3, stride=2, padding=1, output_padding=1))
-        self.layers1.append(nn.ConvTranspose2d(128, 3, 3, stride=2, padding=1, output_padding=1))
+        self.layers1.append(nn.Conv2d(3, 128, 3, stride=1, padding=1))
+        self.layers1.append(nn.Conv2d(128, 256, 3, stride=1, padding=1))
+        self.layers1.append(nn.ConvTranspose2d(256, 128, 3, stride=1, padding=1, output_padding=0))
+        self.layers1.append(nn.ConvTranspose2d(128, 3, 3, stride=1, padding=1, output_padding=0))
 
         # Generator Net 2
         self.layers2 = nn.ModuleList()
-        self.layers2.append(nn.Conv2d(3, 128, 5, stride=2, padding=2))
-        self.layers2.append(nn.Conv2d(128, 256, 3, stride=2, padding=1))
-        self.layers2.append(nn.ConvTranspose2d(256, 128, 3, stride=2, padding=1, output_padding=1))
-        self.layers2.append(nn.ConvTranspose2d(128, 3, 5, stride=2, padding=2, output_padding=1))
+        self.layers2.append(nn.Conv2d(3, 128, 5, stride=1, padding=2))
+        self.layers2.append(nn.Conv2d(128, 256, 3, stride=1, padding=1))
+        self.layers2.append(nn.ConvTranspose2d(256, 128, 3, stride=1, padding=1, output_padding=0))
+        self.layers2.append(nn.ConvTranspose2d(128, 3, 5, stride=1, padding=2, output_padding=0))
 
         # Generator Net 3
         self.layers3 = nn.ModuleList()
-        self.layers3.append(nn.Conv2d(3, 128, 5, stride=2, padding=2))
-        self.layers3.append(nn.Conv2d(128, 256, 3, stride=2, padding=1))
-        self.layers3.append(nn.Conv2d(256, 512, 3, stride=2, padding=1))
-        self.layers3.append(nn.ConvTranspose2d(512, 256, 3, stride=2, padding=1, output_padding=1))
-        self.layers3.append(nn.ConvTranspose2d(256, 128, 3, stride=2, padding=1, output_padding=1))
-        self.layers3.append(nn.ConvTranspose2d(128, 3, 5, stride=2, padding=2, output_padding=1))
+        self.layers3.append(nn.Conv2d(3, 128, 5, stride=1, padding=2))
+        self.layers3.append(nn.Conv2d(128, 256, 3, stride=1, padding=1))
+        self.layers3.append(nn.Conv2d(256, 512, 3, stride=1, padding=1))
+        self.layers3.append(nn.ConvTranspose2d(512, 256, 3, stride=1, padding=1, output_padding=0))
+        self.layers3.append(nn.ConvTranspose2d(256, 128, 3, stride=1, padding=1, output_padding=0))
+        self.layers3.append(nn.ConvTranspose2d(128, 3, 5, stride=1, padding=2, output_padding=0))
 
         # Generator Net 4
         self.layers4 = nn.ModuleList()
-        self.layers4.append(nn.Conv2d(3, 128, 7, stride=2, padding=3))
-        self.layers4.append(nn.Conv2d(128, 256, 5, stride=2, padding=2))
-        self.layers4.append(nn.Conv2d(256, 512, 5, stride=2, padding=2))
-        self.layers4.append(nn.ConvTranspose2d(512, 256, 5, stride=2, padding=2, output_padding=1))
-        self.layers4.append(nn.ConvTranspose2d(256, 128, 5, stride=2, padding=2, output_padding=1))
-        self.layers4.append(nn.ConvTranspose2d(128, 3, 7, stride=2, padding=3, output_padding=1))
+        self.layers4.append(nn.Conv2d(3, 128, 7, stride=1, padding=3))
+        self.layers4.append(nn.Conv2d(128, 256, 5, stride=1, padding=2))
+        self.layers4.append(nn.Conv2d(256, 512, 5, stride=1, padding=2))
+        self.layers4.append(nn.ConvTranspose2d(512, 256, 5, stride=1, padding=2, output_padding=0))
+        self.layers4.append(nn.ConvTranspose2d(256, 128, 5, stride=1, padding=2, output_padding=0))
+        self.layers4.append(nn.ConvTranspose2d(128, 3, 7, stride=1, padding=3, output_padding=0))
 
     def forward(self, x):
         model1 = Generator(self.layers1)
-        #model2 = Generator(self.layers2)
-        #model3 = Generator(self.layers3)
-        #model4 = Generator(self.layers4)
-        return model1(x)
+        model2 = Generator(self.layers2)
+        model3 = Generator(self.layers3)
+        model4 = Generator(self.layers4)
+
+        image1 = model1(x)
+        image2 = model2(image1)
+        image3 = model3(image2)
+        image4 = model4(image3)
+
+        return image4
