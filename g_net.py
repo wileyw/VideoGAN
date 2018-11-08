1fcd92e898e73f9dfd0055415cf7afb61cfcb68c
Michael Wang <mswang12@gmail.com>
2018-11-07 20:47:50 -0800

    Adding residual to generator

--- g_net.py
+++ g_net.py
@@ -52,14 +52,14 @@ class GeneratorDefinitions(nn.Module):
 
         # Generator Net 2
         self.layers2 = nn.ModuleList()
-        self.layers2.append(nn.Conv2d(3 * HIST_LEN, 128, 5, stride=1, padding=2))
+        self.layers2.append(nn.Conv2d(3 * (HIST_LEN + 1), 128, 5, stride=1, padding=2))
         self.layers2.append(nn.Conv2d(128, 256, 3, stride=1, padding=1))
         self.layers2.append(nn.ConvTranspose2d(256, 128, 3, stride=1, padding=1, output_padding=0))
         self.layers2.append(nn.ConvTranspose2d(128, 3, 5, stride=1, padding=2, output_padding=0))
 
         # Generator Net 3
         self.layers3 = nn.ModuleList()
-        self.layers3.append(nn.Conv2d(3 * HIST_LEN, 128, 5, stride=1, padding=2))
+        self.layers3.append(nn.Conv2d(3 * (HIST_LEN + 1), 128, 5, stride=1, padding=2))
         self.layers3.append(nn.Conv2d(128, 256, 3, stride=1, padding=1))
         self.layers3.append(nn.Conv2d(256, 512, 3, stride=1, padding=1))
         self.layers3.append(nn.ConvTranspose2d(512, 256, 3, stride=1, padding=1, output_padding=0))
@@ -68,29 +68,38 @@ class GeneratorDefinitions(nn.Module):
 
         # Generator Net 4
         self.layers4 = nn.ModuleList()
-        self.layers4.append(nn.Conv2d(3 * HIST_LEN, 128, 7, stride=1, padding=3))
+        self.layers4.append(nn.Conv2d(3 * (HIST_LEN + 1), 128, 7, stride=1, padding=3))
         self.layers4.append(nn.Conv2d(128, 256, 5, stride=1, padding=2))
         self.layers4.append(nn.Conv2d(256, 512, 5, stride=1, padding=2))
         self.layers4.append(nn.ConvTranspose2d(512, 256, 5, stride=1, padding=2, output_padding=0))
         self.layers4.append(nn.ConvTranspose2d(256, 128, 5, stride=1, padding=2, output_padding=0))
         self.layers4.append(nn.ConvTranspose2d(128, 3, 7, stride=1, padding=3, output_padding=0))
 
+        self.model1 = Generator(self.layers1)
+        self.model2 = Generator(self.layers2)
+        self.model3 = Generator(self.layers3)
+        self.model4 = Generator(self.layers4)
+
+
     def forward(self, x):
-        model1 = Generator(self.layers1)
-        model2 = Generator(self.layers2)
-        model3 = Generator(self.layers3)
-        model4 = Generator(self.layers4)
+        model1 = self.model1
+        model2 = self.model2
+        model3 = self.model3
+        model4 = self.model4
 
         input_image1 = F.interpolate(x, size=(4, 4))
         image1 = model1(input_image1)
+        upsampled_image1 = F.interpolate(image1, size=(8, 8))
 
-        input_image2 = F.interpolate(x, size=(8, 8))
-        image2 = model2(input_image2)
+        input_image2 = torch.cat([F.interpolate(x, size=(8, 8)), upsampled_image1], 1)
+        image2 = model2(input_image2) + upsampled_image1
+        upsampled_image2 = F.interpolate(image2, size=(16, 16))
 
-        input_image3 = F.interpolate(x, size=(16, 16))
-        image3 = model3(input_image3)
+        input_image3 = torch.cat([F.interpolate(x, size=(16, 16)), upsampled_image2], 1)
+        image3 = model3(input_image3) + upsampled_image2
+        upsampled_image3 = F.interpolate(image3, size=(32, 32))
 
-        input_image4 = F.interpolate(x, size=(32, 32))
-        image4 = model4(input_image4)
+        input_image4 = torch.cat([F.interpolate(x, size=(32, 32)), upsampled_image3], 1)
+        image4 = model4(input_image4) + upsampled_image3
 
         return image4
