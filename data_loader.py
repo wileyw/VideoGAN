358a928351d9aaeac03d8e583169cc48798ed318
jinac <john@ditto.com>
2019-01-09 20:06:36 -0800

    Adding DataLoader WIP

---
+++ data_loader.py
@@ -0,0 +1,40 @@
+import os
+import numpy as np
+
+
+class DataLoader:
+    def __init__(self,
+                 train_dir_clips,
+                 num_clips,
+                 batch_size,
+                 train_height,
+                 train_width,
+                 hist_len):
+        self.batch_size = batch_size
+        self.train_height = train_height
+        self.train_width = train_width
+        self.hist_len = hist_len
+        self.num_clips = num_clips
+        self.train_dir_clips = train_dir_clips
+
+    def get_train_batch():
+        """
+        Loads batch_size clips from the database of preprocessed training clips.
+
+        @return: An array of shape
+                [batch_size, train_height, train_width, (3 * (hist_len + 1))].
+        """
+        clips = np.empty([self.batch_size,
+                          self.train_height,
+                          self.train_width,
+                          (3 * (self.hist_len + 1))],
+                         dtype=np.float32)
+        for i in xrange(batch_size):
+            path = (self.trian_dir_clips
+                    str(np.random.choice(c.NUM_CLIPS))
+                    '.npz')
+            clip = np.load(path)['arr_0']
+
+            clips[i] = clip
+
+        return clips
