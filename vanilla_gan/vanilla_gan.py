c9b143cb828ed9bd45548d8e70088d927bebb9b5
Michael Wang <mswang12@gmail.com>
2019-02-16 19:45:40 -0800

    Add resnet skip connections to Vanilla GAN generator

--- vanilla_gan/vanilla_gan.py
+++ vanilla_gan/vanilla_gan.py
@@ -89,3 +89,97 @@ class Generator(nn.Module):
         out = torch.tanh(out)
 
         return out
+
+class GeneratorSkipConnections(nn.Module):
+    def __init__(self):
+        super(GeneratorSkipConnections, self).__init__()
+
+        # Upsampling layer
+        self.deconv1 = nn.ConvTranspose2d(100, 128, 4, stride=4, padding=0)
+        nn.init.xavier_normal(self.deconv1.weight)
+
+        # Resnet block
+        self.deconv1A = nn.ConvTranspose2d(128, 128, 3, stride=1, padding=1)
+        nn.init.xavier_normal(self.deconv1A.weight)
+        self.bn1 = nn.BatchNorm2d(128)
+        self.deconv1B = nn.ConvTranspose2d(128, 128, 3, stride=1, padding=1)
+        nn.init.xavier_normal(self.deconv1B.weight)
+
+        # Upsampling layer
+        self.deconv2 = nn.ConvTranspose2d(128, 64, 4, stride=2, padding=1)
+        nn.init.xavier_normal(self.deconv2.weight)
+
+        # Resnet block
+        self.deconv2A = nn.ConvTranspose2d(64, 64, 3, stride=1, padding=1)
+        nn.init.xavier_normal(self.deconv2A.weight)
+        self.bn2 = nn.BatchNorm2d(64)
+        self.deconv2B = nn.ConvTranspose2d(64, 64, 3, stride=1, padding=1)
+        nn.init.xavier_normal(self.deconv2B.weight)
+
+        # Upsampling layer 3
+        self.deconv3 = nn.ConvTranspose2d(64, 32, 4, stride=2, padding=1)
+        nn.init.xavier_normal(self.deconv3.weight)
+
+        # Resnet block
+        self.deconv3A = nn.ConvTranspose2d(32, 32, 3, stride=1, padding=1)
+        nn.init.xavier_normal(self.deconv3A.weight)
+        self.bn3 = nn.BatchNorm2d(32)
+        self.deconv3B = nn.ConvTranspose2d(32, 32, 3, stride=1, padding=1)
+        nn.init.xavier_normal(self.deconv3B.weight)
+
+        # Upsampling layer 4
+        self.deconv4 = nn.ConvTranspose2d(32, 3, 4, stride=2, padding=1)
+        nn.init.xavier_normal(self.deconv4.weight)
+
+        # Resnet block
+        self.deconv4A = nn.ConvTranspose2d(3, 3, 3, stride=1, padding=1)
+        nn.init.xavier_normal(self.deconv4A.weight)
+        self.bn4 = nn.BatchNorm2d(3)
+        self.deconv4B = nn.ConvTranspose2d(3, 3, 3, stride=1, padding=1)
+        nn.init.xavier_normal(self.deconv4B.weight)
+
+    def forward(self, x):
+        out = x
+
+        # Multi scale image generation seems quite similar to using ResNet skip connections
+        # In this case, we only use a single Resnet block instead of the entire Generator so the network is small enough to run on my laptop
+        #
+        # Upsample 1
+        out = upsampled = self.deconv1(out)
+        # Resnet block 1
+        out = self.deconv1A(out)
+        out = self.bn1(out)
+        out = F.relu(out)
+        out = self.deconv1B(out)
+        out = upsampled + out
+
+        # Upsample 2
+        out = upsampled = self.deconv2(out)
+        # Resnet block 2
+        out = self.deconv2A(out)
+        out = self.bn2(out)
+        out = F.relu(out)
+        out = self.deconv2B(out)
+        out = upsampled + out
+
+        # Upsample 3
+        out = upsampled = self.deconv3(out)
+        # Resnet block 3
+        out = self.deconv3A(out)
+        out = self.bn3(out)
+        out = F.relu(out)
+        out = self.deconv3B(out)
+        out = upsampled + out
+
+        # Upsample 4
+        out = upsampled = self.deconv4(out)
+        out = torch.tanh(out)
+
+        # Resnet block 4
+        out = self.deconv4A(out)
+        out = self.bn4(out)
+        out = F.relu(out)
+        out = self.deconv4B(out)
+        out = upsampled + out
+
+        return out
