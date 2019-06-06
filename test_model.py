b0bc0723ac58fcc0074c5dfd40739cdf43a8b60d
wileyw <wileywang@gmail.com>
2019-06-05 21:07:53 -0700

    Fix model loading testing tool.

--- test_model.py
+++ test_model.py
@@ -22,6 +22,7 @@ import vanilla_gan.video_gan
 import data_loader
 import loss_funs
 
+from vanilla_gan.video_gan import VideoGANGenerator
 
 
 MODEL_FILEPATH = 'generator_net.pth.tmp'
@@ -31,16 +32,37 @@ CROP_HEIGHT = 32
 CROP_WIDTH = 32
 DTYPE = config.dtype
 
-def test():
+
+def save_samples(generated_images, iteration, prefix):
+    import scipy
+    generated_images = generated_images.data.cpu().numpy()
+
+    num_images, channels, cell_h, cell_w = generated_images.shape
+    ncols = int(np.sqrt(num_images))
+    nrows = int(np.math.floor(num_images / float(ncols)))
+    result = np.zeros((cell_h * nrows, cell_w * ncols, channels), dtype=generated_images.dtype)
+    for i in range(0, nrows):
+        for j in range(0, ncols):
+            result[i*cell_h:(i+1)*cell_h, j*cell_w:(j+1)*cell_w, :] = generated_images[i*ncols+j].transpose(1, 2, 0)
+    grid = result
+
+    if not os.path.exists('output'):
+        os.makedirs('output')
+    scipy.misc.imsave('output/{}_{:05d}.jpg'.format(prefix, iteration), grid)
+
+
+def test_model():
     count = 0
 
     video_g_net = VideoGANGenerator()
     video_g_net.load_state_dict(torch.load(MODEL_FILEPATH))
     video_g_net.eval()
 
+    max_size = len(os.listdir('train'))
+    pacman_dataloader = data_loader.DataLoader('train', min(max_size, 500000), 16, 32, 32, 4)
     clips_x, clips_y = pacman_dataloader.get_train_batch()
-    clips_x = torch.tensor(np.rollaxis(clips_x, 3, 1)).to(device)
-    clips_y = torch.tensor(np.rollaxis(clips_y, 3, 1)).to(device)
+    clips_x = torch.tensor(np.rollaxis(clips_x, 3, 1))
+    clips_y = torch.tensor(np.rollaxis(clips_y, 3, 1))
 
     # batch_size x noise_size x 1 x 1
     batch_size = 16
